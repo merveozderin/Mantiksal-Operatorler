25b061807bc967a2af08fd7816e7abf616fdf06b
Merve <61613791+merveozderin@users.noreply.github.com>
2021-08-15 00:11:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mantıksal operatorler
+
+            bool isSuccess = true ;
+            bool isCompleted = false ;
+
+            if (isSuccess && isCompleted)
+            Console.WriteLine("Perfect") ;
+
+            if (isSuccess || isCompleted)
+            Console.WriteLine("Great") ;
+
+            if (isSuccess && ! isCompleted)
+            Console.WriteLine("Good") ;
+        }
+    }
+}
